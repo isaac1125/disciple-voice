34751bb72f1e930d4fd3620c3a46be2f6f98dfc4
Isaac <desire82023@gmail.com>
2025-04-07 22:35:56 +0800

    Fix SPA routing with 404.html

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './assets/style.css';
+import './style.css';
 
 const app = createApp(App);
 app.use(router);
